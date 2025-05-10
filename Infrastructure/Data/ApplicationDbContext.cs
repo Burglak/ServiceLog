51787f952b8c942bc9b20e9c0a1c8c6e00b2e730
burglak <cyprian.schiffer123@gmail.com>
2025-05-10 21:57:06 +0200

    feat(infrastructure): add ApplicationDbContext for database context management

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceLog.Domain.Entities;
+
+namespace ServiceLog.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<ServiceRecord> ServiceRecords { get; set; }
+        public DbSet<ServiceRecordImage> ServiceRecordImages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleImage> VehicleImages { get; set; }
+        public DbSet<VehicleUser> VehicleUsers { get; set; }
+
+    // automatically sets createdAt for newly added entities and updatedAt for modified entities
+    public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries()
+                .Where(e => e.Entity is BaseEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entry.Entity).CreatedAt = DateTime.UtcNow;
+                }
+                ((BaseEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
+            }
+
+            return base.SaveChanges();
+        }
+
+    }
+}
