4937f1f9116b561d4ffa25a139b6c1121c0d9ab8
burglak <cyprian.schiffer123@gmail.com>
2025-05-13 02:48:34 +0200

    refractor: add repository registration

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using ServiceLog.Domain.Entities;
 using ServiceLog.Infrastructure.Data;
+using ServiceLog.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register repositories
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<VehicleRepository>();
+builder.Services.AddScoped<ServiceRecordRepository>();
+builder.Services.AddScoped<NotificationRepository>();
+builder.Services.AddScoped<VehicleImageRepository>();
+builder.Services.AddScoped<VehicleUserRepository>();
 
 var app = builder.Build();
 
